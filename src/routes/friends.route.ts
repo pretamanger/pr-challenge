38369abccae565a3439b64bba1aa2ec10664097b
Joe Brady <ext_josephmb@pret.com>
2025-09-24 13:26:13 +0100

    Add recursion for friends tree

--- src/routes/friends.route.ts
+++ src/routes/friends.route.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { type AxiosResponse } from "axios";
 import type { RequestHandler } from "express";
 import { EXTERNAL_FRIENDS_SERVICE_BASE_URL } from "../../external-friends-service/constants.js";
 
@@ -9,9 +9,49 @@ export const friendsRoute: RequestHandler = async (req, res) => {
     return res.status(400);
   }
 
-  const friends = await axios.get(
+  const friends = (await axios.get(
     `${EXTERNAL_FRIENDS_SERVICE_BASE_URL}/external-friends-service?userId=${userId}`
-  );
+  )) as AxiosResponse<{ id: string; name: string }[]>;
 
-  return res.json(friends.data);
+  const visitedFriends = new Set<string>([userId]);
+
+  const getFriendsRecursively = async (
+    listOfFriends: { id: string; name: string }[]
+  ) => {
+    type FriendsTreeNode = {
+      id: string;
+      name: string;
+      friends: FriendsTreeNode[];
+    };
+
+    let friendsTree = [];
+
+    for (let friend of listOfFriends) {
+      if (visitedFriends.has(friend.id)) {
+        continue;
+      }
+      visitedFriends.add(friend.id);
+
+      const friends = (
+        await axios.get(
+          `${EXTERNAL_FRIENDS_SERVICE_BASE_URL}/external-friends-service?userId=${friend.id}`
+        )
+      ).data as unknown;
+
+      const friendNode: FriendsTreeNode = {
+        id: friend.id,
+        name: friend.name,
+        // @ts-ignore
+        friends: await getFriendsRecursively(await friends),
+      };
+
+      friendsTree.push(friendNode);
+    }
+
+    return friendsTree;
+  };
+
+  const friendsTree = await getFriendsRecursively(friends.data);
+
+  return res.json(friendsTree);
 };
